f7dd5e0c20baa8e5a61b4d88183bc432c59ec85c
Edson_LMarques <edsonmarques@gea.inatel.br>
2021-09-28 21:26:36 -0300

    v2 - fuzzy simples Implantação do sistema da base de regras e formula de saida

--- v2/fuzzy.py
+++ v2/fuzzy.py
@@ -6,53 +6,88 @@ class Simple_Fuzzy:
         self.saida = 0
         self.parametros_entrada = [-10, -5, -1, 1, 5, 10]
         self.parametros_saida = [0, 25, 50, 75, 100]
-        self.base = [["MB", "B", "M", "M", "A", "MA"]]
+        self.base = ["MB", "B", "M", "A", "MA", "MA"]
 
     def calcula_fuzzy(self, valor):
-        grau = self.calcula_grau(valor)
-        saida = self.calcula_saida(grau)
+        self.calcula_grau(valor)
+        saida = self.calcula_saida()
+        print(f'''saida = {saida}\n\n''')
         return saida
 
     def calcula_grau(self, valor):
         MN, N, PN, PP, P, MP = self.parametros_entrada
         grau = [0, 0, 0, 0, 0, 0]
-        if valor <= MN:
+        if valor < MN:
             grau[0] = 1
-        elif MN < valor <= N:
+        elif MN <= valor < N:
             A = MN
             B = N
-            grau[0] = (1 / (B - A)) * valor + (A / (A - B))
-            grau[1] = (1 / (A - B)) * valor + (B / (B - A))
-        elif N < valor <= PN:
+            grau[1] = (1 / (B - A)) * valor + (A / (A - B))
+            grau[0] = (1 / (A - B)) * valor + (B / (B - A))
+        elif N <= valor < PN:
             A = N
             B = PN
-            grau[1] = (1 / (B - A)) * valor + (A / (A - B))
-            grau[2] = (1 / (A - B)) * valor + (B / (B - A))
-        elif PN < valor <= PP:
+            grau[2] = (1 / (B - A)) * valor + (A / (A - B))
+            grau[1] = (1 / (A - B)) * valor + (B / (B - A))
+        elif PN <= valor < PP:
             A = PN
             B = PP
-            grau[2] = (1 / (B - A)) * valor + (A / (A - B))
-            grau[3] = (1 / (A - B)) * valor + (B / (B - A))
-        elif PP < valor <= P:
+            grau[3] = (1 / (B - A)) * valor + (A / (A - B))
+            grau[2] = (1 / (A - B)) * valor + (B / (B - A))
+        elif PP <= valor < P:
             A = PP
             B = P
-            grau[3] = (1 / (B - A)) * valor + (A / (A - B))
-            grau[4] = (1 / (A - B)) * valor + (B / (B - A))
-        elif P < valor <= MP:
+            grau[4] = (1 / (B - A)) * valor + (A / (A - B))
+            grau[3] = (1 / (A - B)) * valor + (B / (B - A))
+        elif P <= valor <= MP:
             A = P
             B = MP
-            grau[4] = (1 / (B - A)) * valor + (A / (A - B))
-            grau[5] = (1 / (A - B)) * valor + (B / (B - A))
-        else:
+            grau[5] = (1 / (B - A)) * valor + (A / (A - B))
+            grau[4] = (1 / (A - B)) * valor + (B / (B - A))
+        elif valor > MP:
             grau[5] = 1
-            return grau
+        self.grau = grau
+        return grau
 
-    def calcula_saida(self, grau):
-        # base
-        # Base =
-        # base = grau
-        # saida
-        pass
+    def calcula_saida(self):
+        base = self.base
+        grau = self.grau
+        matriz = zip(base, grau)
+        matriz = list(matriz)
+        MB = []
+        B = []
+        M = []
+        A = []
+        MA = []
+        for item in matriz:
+            if item[0] == "MB":
+                MB.append(item[1])
+            elif item[0] == "B":
+                B.append(item[1])
+            elif item[0] == "M":
+                M.append(item[1])
+            elif item[0] == "A":
+                A.append(item[1])
+            elif item[0] == "MA":
+                MA.append(item[1])
+        MB = max(MB)
+        B = max(B)
+        M = max(M)
+        A = max(A)
+        MA = max(MA)
+        grau_saida = [MB, B, M, A, MA]
+        parametros_saidas = self.parametros_saida
+        print(f'''grau saida = {grau_saida}''')
+        try: #rever essa formmula
+            saida = ((parametros_saidas[0]+1) * MB + (parametros_saidas[1]) * B +
+                     (parametros_saidas[2]) * M + (parametros_saidas[3]) * A +
+                     (parametros_saidas[4]) * MA) \
+                    / (MB + B + M + A + MA)
+            self.grau_saida = grau_saida
+            self.saida = saida
+            return saida
+        except:
+            print("divisão por 0")
 
     def get_grau(self):
         return self.grau
@@ -60,7 +95,7 @@ class Simple_Fuzzy:
     def get_grau_saida(self):
         return self.get_grau_saida
 
-    def saida(self):
+    def get_saida(self):
         return self.saida
 
     def set_parametros_entrada(self, parametros):
--- v2/main.py
+++ v2/main.py
@@ -1,12 +1,13 @@
 #simple fuzzy that defines how much something is
-#
-#
-
-
+import random
 
 from fuzzy import Simple_Fuzzy
+from random import *
 
+saida = []
 fuzzy = Simple_Fuzzy()
-for i in range(-11,11):
-    saida = fuzzy.calcula_fuzzy(i)
+for i in range(-10,11):
+    x = randrange(-10, 9) + randrange(0, 10)/10 + randrange(0, 10)/100
+    print(f'''valor = {x}''')
+    saida.append(fuzzy.calcula_fuzzy(x))
 print(saida)
